3c492e0e14490a1ace39c1710e231b81add5d37b
clcar23 <92751822+clcar23@users.noreply.github.com>
2021-11-10 17:50:15 -0700

    created JS project
    created simple JS project and html

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var Family = "Marx Brothers", oldest ="Chico", middle = "Harpo", youngest = "Groucho";
+var oldest = oldest.fontcolor("yellow");
+var youngest = youngest.fontcolor("green");
+var middle = middle.fontcolor("purple");
+var Family = Family.fontcolor("red");//this assigns the font color for family to red//
+document.write(Family);//this prints the value of family//
+document.write(oldest);
+document.write(middle);
+window.alert("Here's a random message!");
+var one = "I think I'm";//here's a string//
+var two = " going crazy!";//and another string//
+document.write(one + two);//and here's them put together//
+3 > 1; //here's an expression//
