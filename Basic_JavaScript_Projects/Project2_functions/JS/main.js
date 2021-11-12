b3a89c97b4d59c556263d2a3c3cea09a1d552840
clcar23 <92751822+clcar23@users.noreply.github.com>
2021-11-11 18:38:57 -0700

    created JS functions
    created JS functions and html file

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function deadBug() {
+    document.getElementById().innerHTML = "Now it's dead!"//Create deadBug function and will change text to Now its dead in the html//
+}
+
+function myFunction() {
+    var sentence = "This part is ";
+    sentence += " so very frustrating!";
+    document.getElementById("concatenate").innerHTML = sentence;//function that joins two sentences//
+}
