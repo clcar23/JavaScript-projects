f1ca1d7ead213c6642f5489a64fc1172d1a57872
clcar23 <92751822+clcar23@users.noreply.github.com>
2021-11-14 17:18:15 -0700

    Project5 comparisons
    created HTML and JS files

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,23 @@
+function my_Function() {
+    document.write(3E310); /*-this value will return infinity-*/
+    document.write(-3E310); /*-this value will return a negative infinity-*/
+    document.write(10>2); /*-this value will return true-*/
+    console.log(5 * 5); /*-this will return 25 to the console-*/
+    console.log(5 == 7); /*-this will return FALSE to the console-*/
+    document.write(7 == 13); /*-this will return FALSE-*/
+}
+
+function my_Function2() {
+    document.write(10 === (2 * 5)); /*-this will return TRUE-*/
+    document.write(10 === 'dime'); /*-this will return FALSE-*/
+    document.write('five' === 5); /*-this will return FALSE-*/
+    document.write(3 === (2 + 2)); /*-this will return FALSE-*/
+    document.write(5 > 3 && 7 < 12); /*-this will return TRUE, both are TRUE-*/
+    document.write(3 < 5 && 12 > 4); /*-this will return FALSE bc one value in FALSE-*/
+    document.write(3 < 6 || 8 > 4); /*-this will return TRUE bc one value is TRUE-*/
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(30 > 10); /*- this line will return FALSE-*/
+    document.getElementById("Not").innerHTML = !(4 > 11); /*-combined with the first line, this will return TRUE bc it is a double negative -*/
+}
